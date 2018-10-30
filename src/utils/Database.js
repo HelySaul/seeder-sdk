b7e4600b83157e77ea6a60d9b2b12d5b7566279e
denimar <denimar@gmail.com>
2018-10-30 17:29:05 -0300

    Adding method Database.updateDb

--- src/utils/Database.js
+++ src/utils/Database.js
@@ -27,6 +27,18 @@ class Database {
     return dbConnection
   }
 
+  async updateDb(couchDb, databaseName, object, partial) {
+    if (!object._id) {
+      throw new Error('Field "_id" is required')
+    }
+    let objectToUpdate = object
+    if (!objectToUpdate._rev || partial) {
+      const currentObj = await couchDb.get(databaseName, object._id)
+      objectToUpdate = Object.assign(currentObj.data, object)
+    }
+    return await couchDb.update(databaseName, objectToUpdate)
+  }  
+
   static async reconnect(err) {
     logger.error(`Database not found: ${err}, trying to find in 3 seconds...`)
     setTimeout(async () => await Database.connect(), 3000)
