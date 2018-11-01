d2b5cfa0a93768f85312f9c45de4d4618d5f39fb
denimar <denimar@gmail.com>
2018-11-01 18:12:03 -0300

    Added GoogleStorage class

---
+++ src/utils/GoogleStorage.js
@@ -0,0 +1,36 @@
+import logger from './logger'
+
+class GoogleStorage {
+
+  async uploadFile(bucket, userId, fileName, file) {
+    return new Promise((resolve, reject) => {
+      try {
+        const bucketFile = bucket.file(`${userId}/${fileName}`)
+        const stream = bucketFile.createWriteStream({
+          metadata: {
+            contentType: file.mimetype
+          }
+        })
+        stream.on('error', err => {
+          bucketFile.cloudStorageError = err
+          logger.error('Error uploading file : ' + JSON.stringify(err))
+          reject(err)
+        })
+        stream.on('finish', par1 => {
+          bucketFile.cloudStorageObject = fileName
+          //return bucketFile.makePublic().then(() => {
+          //bucketFile.gcsUrl = `https://storage.googleapis.com/smash-profile-file/${userId}/{fileName}`
+          resolve()
+          //})
+        })
+        stream.end(file.data)
+      } catch (error) {
+        reject(error)
+        logger.error('Error uploading file : ' + JSON.stringify(error))
+      }
+    })
+  }
+
+}
+
+export default GoogleStorage
