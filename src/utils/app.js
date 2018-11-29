afd9c80a925caad66a252a6dc3207e5c7c89b749
denimar <denimar@gmail.com>
2018-11-29 18:06:16 -0200

    fixing CORS

--- src/utils/app.js
+++ src/utils/app.js
@@ -9,11 +9,26 @@ import fileUpload from 'express-fileupload'
 const app = express()
 const token = new Token()
 
+const whitelist = ['http://192.168.86.22:4200', 'http://192.168.86.35:4200']    
+const options = {
+  origin: function (origin, callback) {
+    logger.info(origin)
+    
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}    
+
 app
   .use(morgan(Env.HTTP_LOG_CONFIG, { stream: logger.stream }))
-  .use((req, res, next) => {
-    console.log(req.headers.origin)
 
+  .use(cors(options))    
+
+  /*
+  .use((req, res, next) => {
     // To use with websocket, uncomment this line below and comment the next line
     res.header('Access-Control-Allow-Origin', req.headers.origin + ', http://192.168.86.22:4200')
     //res.header('Access-Control-Allow-Origin', '*')
@@ -22,6 +37,7 @@ app
     res.header('Access-Control-Allow-Headers', '*')
     next()
   })
+  */
   .use(token.httpValidate)
   .use(json({ limit: '5mb' }))
   .use(
