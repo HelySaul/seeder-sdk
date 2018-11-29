58c2737a75c59e741605ff2a0b70e8105711c39b
denimar <denimar@gmail.com>
2018-11-29 19:56:58 -0200

    fixing CORS

--- src/utils/app.js
+++ src/utils/app.js
@@ -25,31 +25,28 @@ const options = {
 }
 */
 
-var corsOptions = {
-  origin: 'http://192.168.86.22:4200',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+// var corsOptions = {
+//   origin: 'http://192.168.86.22:4200',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
 app
   .use(morgan(Env.HTTP_LOG_CONFIG, { stream: logger.stream }))
 
-  .use(cors(corsOptions))    
+  //.use(cors(corsOptions))    
 
-  /*
   .use((req, res, next) => {
     logger.info('SDK Origin -> ' + req.headers.origin)
 
-
     // To use with websocket, uncomment this line below and comment the next line
-    res.header('Access-Control-Allow-Origin', 'http://192.168.86.22:4200')
-    //res.header('Access-Control-Allow-Origin', '*')
+    //res.header('Access-Control-Allow-Origin', 'http://192.168.86.22:4200')
+    res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Credentials', true)
     res.header('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE')
     res.header('Access-Control-Allow-Headers', '*')
     next()
   })
-  */
-
+  
   .use(token.httpValidate)
   .use(json({ limit: '5mb' }))
   .use(
