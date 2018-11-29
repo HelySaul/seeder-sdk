23b633aef4cbeeed6564122f460583e970f5798f
denimar <denimar@gmail.com>
2018-11-29 18:41:35 -0200

    fixing CORS

--- src/utils/app.js
+++ src/utils/app.js
@@ -29,8 +29,8 @@ app
 
   .use((req, res, next) => {
     // To use with websocket, uncomment this line below and comment the next line
-    //res.header('Access-Control-Allow-Origin', req.headers.origin + ', http://192.168.86.22:4200')
-    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Origin', req.headers.origin)
+    //res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Credentials', true)
     res.header('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE')
     res.header('Access-Control-Allow-Headers', '*')
