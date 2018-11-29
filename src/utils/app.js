0fa4883b854c3b76984fcd98bc33c1c68e296104
denimar <denimar@gmail.com>
2018-11-29 18:35:58 -0200

    fixing CORS

--- src/utils/app.js
+++ src/utils/app.js
@@ -25,19 +25,18 @@ const options = {
 app
   .use(morgan(Env.HTTP_LOG_CONFIG, { stream: logger.stream }))
 
-  .use(cors())    
+  //.use(cors())    
 
-  /*
   .use((req, res, next) => {
     // To use with websocket, uncomment this line below and comment the next line
-    res.header('Access-Control-Allow-Origin', req.headers.origin + ', http://192.168.86.22:4200')
-    //res.header('Access-Control-Allow-Origin', '*')
+    //res.header('Access-Control-Allow-Origin', req.headers.origin + ', http://192.168.86.22:4200')
+    res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Credentials', true)
     res.header('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE')
     res.header('Access-Control-Allow-Headers', '*')
     next()
   })
-  */
+
   .use(token.httpValidate)
   .use(json({ limit: '5mb' }))
   .use(
